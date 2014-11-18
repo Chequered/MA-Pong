264396a5f8769b81ab070a06602b0d358c6906d0
Daan Ruiter <daanruiter@live.nl>
2014-11-18 12:29:38 +0100

    added healthbars to enemies with fadeout

--- The Long Run/The Long Run/Assets/_Scripts/EnemyController.cs
+++ The Long Run/The Long Run/Assets/_Scripts/EnemyController.cs
@@ -3,31 +3,39 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+	public GameObject healthBar;
 	public Transform target;
 	public float speed;
 	public float hp;
 	public float damage;
 
 	void Start () {
-	
+		healthBar.GetComponent<Healthbar>().SetHealth(hp);
 	}
 
 	void Update () {
 		float walk = speed * Time.deltaTime;
 		transform.parent.transform.position = Vector3.MoveTowards (transform.position, target.position, walk);
+		healthBar.GetComponent<Healthbar>().UpdatePosition(transform.position);
 	}
 
 	void OnTriggerEnter(Collider coll)
 	{
-		Debug.Log(coll.tag);
 		if(coll.transform.tag == "Bullet")
 		{
-			this.hp -= coll.GetComponent<BulletBase>().GetDamage();
-			Destroy(coll.gameObject);
-			
-			if (hp < 1) {
-				Destroy(this.gameObject);
-			}
+			TakeDamage(coll.gameObject);
+		}
+	}
+
+	private void TakeDamage(GameObject bullet)
+	{
+		this.hp -= bullet.GetComponent<BulletBase>().GetDamage();
+		Destroy(bullet.gameObject);
+
+		healthBar.GetComponent<Healthbar>().UpdateBar(this.hp);
+
+		if (hp < 1) {
+			Destroy(transform.parent.gameObject);
 		}
 	}
 }
--- The Long Run/The Long Run/Assets/_Scripts/Healthbar.cs
+++ The Long Run/The Long Run/Assets/_Scripts/Healthbar.cs
@@ -4,21 +4,62 @@ using System.Collections;
 public class Healthbar : MonoBehaviour {
 
 	public GameObject greenBar;
+	public GameObject redBar;
 
 	private float maxHealth;
 	private float currentHealth;
 
 	private float maxWidth;
+	private bool visible;
+
+	private Color normalGreen;
+	private Color normalRed;
+
+	private void Start()
+	{
+		normalRed = redBar.guiTexture.color;
+		normalGreen = greenBar.guiTexture.color;
+
+		Color green = greenBar.guiTexture.color;
+		green.a = 0f;
+		Color red = redBar.guiTexture.color;
+		red.a = 0f;
+		greenBar.guiTexture.color = green;
+		redBar.guiTexture.color = red;
+	}
 
 	public void SetHealth(float _max)
 	{
 		maxHealth = _max;
 		currentHealth = _max;
+		maxWidth = greenBar.guiTexture.pixelInset.width;
+	}
+
+	public void UpdatePosition(Vector3 pos)
+	{
+		Vector3 barPos = Camera.main.WorldToViewportPoint (pos);
+		transform.position = barPos;
 	}
 
 	public void UpdateBar(float _new)
 	{
 		currentHealth = _new;
-		greenBar.GetComponent<GUITexture>().guiTexture.pixelInset.width = maxWidth * currentHealth / maxHealth;
+		visible = true;
+		greenBar.guiTexture.pixelInset = new Rect(greenBar.guiTexture.pixelInset.x, greenBar.guiTexture.pixelInset.y, maxWidth * currentHealth / maxHealth, greenBar.guiTexture.pixelInset.height);
+		greenBar.guiTexture.color = normalGreen;
+		redBar.guiTexture.color = normalRed;
+	}
+
+	private void Update()
+	{
+		if(visible)
+		{
+			Color green = greenBar.guiTexture.color;
+			green.a -= 0.0035f;
+			Color red = redBar.guiTexture.color;
+			red.a -= 0.0035f;
+			greenBar.guiTexture.color = green;
+			redBar.guiTexture.color = red;
+		}
 	}
 }
