4a1173ff942189b574e60feed957539cd622f17c
rob verhoef <16617@ma-web.nl>
2014-11-24 09:00:14 +0100

    need to pull

---
+++ The Long Run/The Long Run/Assets/_Scripts/AssignTag.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssignTag : MonoBehaviour 
+{
+	
+	void onTriggerStay(Collider other)
+	{
+		if(other.gameObject.tag == "playerOutside")
+		{
+			other.gameObject.tag = "playerInside";
+		}
+	}
+}
